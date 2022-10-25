36b1153af23123587e479efa57da286f041d799e
dlstechteaching <dls.tech.teaching@gmail.com>
2022-10-25 15:05:25 +0200

    test(services): adding unit tests for all services functions

--- src/services/login.js
+++ src/services/login.js
@@ -2,9 +2,9 @@ const fakeLoginUsername = "admin";
 const fakeLoginPassword = "admin";
 
 module.exports = function (username, password) {
-	if (!username || !password) {
-		return { error: "Empty credentials" };
-	}
+    if (!username || !password) {
+        return { error: "Empty credentials" };
+    }
     if (username === fakeLoginUsername && password === fakeLoginPassword) {
         return `Success !!!`;
     }
